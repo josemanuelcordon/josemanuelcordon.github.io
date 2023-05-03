ba18f734f8be793972a00935c623dd93156e9f69
josemanuelcordon <josemanuelcordoncastillo01@gmail.com>
2023-05-03 11:25:32 +0200

    nav-bar test 1 responsive

--- index.js
+++ index.js
@@ -94,13 +94,17 @@ const swiper = new Swiper('.swiper', {
   
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
+        el: '.swiper-pagination',
     },
   
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    effect: 'flip',
+    flipEffect: {
+        slideShadows: false,
+    },
   });
   
